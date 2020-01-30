6dc4826090a5cc9ace2bd5945c9bd202e19bf5a8
helmy <ashraf.h@onthegosystems.com>
2020-01-30 09:08:09 +0200

    add serve repo

---
+++ build/precache-manifest.bbc2816f2568479b3a3b18d017f0c328.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb987011816b7865b4f3b9553919a6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa6b7d1cc866b3440f3c",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "14bfc534bd91c6b8a392",
+    "url": "/static/css/main.ae80000b.chunk.css"
+  },
+  {
+    "revision": "fa6b7d1cc866b3440f3c",
+    "url": "/static/js/2.9f484729.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.9f484729.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14bfc534bd91c6b8a392",
+    "url": "/static/js/main.01f6258e.chunk.js"
+  },
+  {
+    "revision": "0882e3cc6e640ec03f8a",
+    "url": "/static/js/runtime-main.03a36221.js"
+  }
+]);
